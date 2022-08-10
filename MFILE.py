7b9e81f48348a258e49411b14e2216da739881e4
FAHAD HOSSAIN REFAT <109792845+REFAT-156@users.noreply.github.com>
2022-08-10 12:22:30 +0600

    Create MFILE.py

---
+++ MFILE.py
@@ -0,0 +1,30 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from RDUMP64 import __login
+ 
+        __login()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from RDUMP32 import __login
+ 
+ 
+        __login()
